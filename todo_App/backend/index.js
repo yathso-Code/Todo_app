aa507d1ff9a12ef91b9ab812c9d380f89685a0b3
yatharth <yatharth@immutabledevs.com>
2025-07-06 23:44:29 +0530

    add testing roout edit

--- todo_App/backend/index.js
+++ todo_App/backend/index.js
@@ -21,7 +21,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 // ✅ Test Route
 app.get('/api/test', (req, res) => {
-  res.status(200).json({ message: 'API is working ✅' });
+  res.status(200).json({ message: 'API is working ..' });
 });
 app.use(errorHandler);
 
