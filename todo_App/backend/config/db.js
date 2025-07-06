a93a47a49a25a96466187e8503bbcd10c6a787c8
yatharth <yatharth@immutabledevs.com>
2025-07-06 23:41:07 +0530

    add vercel.json file

--- todo_App/backend/config/db.js
+++ todo_App/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://yatharthshukla004:sangeetdilipyesh%402002@cluster0.aake8nm.mongodb.net/todoList?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('< ======== Database is connected =============>');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
--- todo_App/backend/utils/generateToken.js
+++ todo_App/backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, "your_jwt_secret_key", {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: '7d',
   });
 };
