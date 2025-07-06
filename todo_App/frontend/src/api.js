371b9454a8e4a49cb61b11dc32d08c5ddca776d5
yatharth <yatharth@immutabledevs.com>
2025-07-07 00:36:13 +0530

    change on url api

--- todo_App/frontend/src/api.js
+++ todo_App/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://todo-app-amber-alpha.vercel.app/api"
 });
 
 API.interceptors.request.use((req) => {
