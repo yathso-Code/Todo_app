219758e09d839b2080e93d8c23c3c38db2277af9
yatharth <yatharth@immutabledevs.com>
2025-07-12 23:44:48 +0530

    Temporary commit before merge

--- todo_App/backend/index.js
+++ todo_App/backend/index.js
@@ -34,7 +34,7 @@ app.use('/api/tasks', taskRoutes);
 app.get('/api/test', (req, res) => {
   res.status(200).json({ message: 'API is working ..' });
 });
-app.use(errorHandler);
+app.use(errorHandler);git
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`< ======== Server running on port ${PORT} +===============>`));
